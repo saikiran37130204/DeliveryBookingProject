d1eca878fa7c850f84f2d5ce1552e68500308c71
Kondreddy Saikiran reddy <67851495+saikiran37130204@users.noreply.github.com>
2021-05-25 19:01:07 +0530

    Add files via upload

---
+++ DeliveryBookingSystemAPI/Controllers/BookingsController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using DeliveryBookingSystemAPI.Models;
+
+namespace DeliveryBookingSystemAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookingsController : ControllerBase
+    {
+        private readonly DeliveryContext _context;
+
+        public BookingsController(DeliveryContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Bookings
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Bookings>>> Getbookings()
+        {
+            return await _context.bookings.ToListAsync();
+        }
+
+        // GET: api/Bookings/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Bookings>> GetBookings(int id)
+        {
+            var bookings = await _context.bookings.FindAsync(id);
+
+            if (bookings == null)
+            {
+                return NotFound();
+            }
+
+            return bookings;
+        }
+
+        // PUT: api/Bookings/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutBookings(int id, Bookings bookings)
+        {
+            if (id != bookings.orderID)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(bookings).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!BookingsExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Bookings
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Bookings>> PostBookings(Bookings bookings)
+        {
+            _context.bookings.Add(bookings);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetBookings", new { id = bookings.orderID }, bookings);
+        }
+
+        // DELETE: api/Bookings/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteBookings(int id)
+        {
+            var bookings = await _context.bookings.FindAsync(id);
+            if (bookings == null)
+            {
+                return NotFound();
+            }
+
+            _context.bookings.Remove(bookings);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool BookingsExists(int id)
+        {
+            return _context.bookings.Any(e => e.orderID == id);
+        }
+    }
+}
---
+++ DeliveryBookingSystemAPI/Controllers/DeliveryExecutiveController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using DeliveryBookingSystemAPI.Models;
+
+namespace DeliveryBookingSystemAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DeliveryExecutiveController : ControllerBase
+    {
+        private readonly DeliveryContext _context;
+
+        public DeliveryExecutiveController(DeliveryContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/DeliveryExecutive
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<DeliveryExecutive>>> GetdeliveryExecutives()
+        {
+            return await _context.deliveryExecutives.ToListAsync();
+        }
+
+        // GET: api/DeliveryExecutive/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<DeliveryExecutive>> GetDeliveryExecutive(int id)
+        {
+            var deliveryExecutive = await _context.deliveryExecutives.FindAsync(id);
+
+            if (deliveryExecutive == null)
+            {
+                return NotFound();
+            }
+
+            return deliveryExecutive;
+        }
+
+        // PUT: api/DeliveryExecutive/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutDeliveryExecutive(int id, DeliveryExecutive deliveryExecutive)
+        {
+            if (id != deliveryExecutive.executiveID)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(deliveryExecutive).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!DeliveryExecutiveExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/DeliveryExecutive
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<DeliveryExecutive>> PostDeliveryExecutive(DeliveryExecutive deliveryExecutive)
+        {
+            _context.deliveryExecutives.Add(deliveryExecutive);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetDeliveryExecutive", new { id = deliveryExecutive.executiveID }, deliveryExecutive);
+        }
+
+        // DELETE: api/DeliveryExecutive/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteDeliveryExecutive(int id)
+        {
+            var deliveryExecutive = await _context.deliveryExecutives.FindAsync(id);
+            if (deliveryExecutive == null)
+            {
+                return NotFound();
+            }
+
+            _context.deliveryExecutives.Remove(deliveryExecutive);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool DeliveryExecutiveExists(int id)
+        {
+            return _context.deliveryExecutives.Any(e => e.executiveID == id);
+        }
+    }
+}

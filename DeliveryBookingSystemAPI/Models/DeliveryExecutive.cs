39200397e01432cba42ee757ac3e93f4eff1b51b
Kondreddy Saikiran reddy <67851495+saikiran37130204@users.noreply.github.com>
2021-05-25 18:58:46 +0530

    Add files via upload

---
+++ DeliveryBookingSystemAPI/Models/DeliveryExecutive.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DeliveryBookingSystemAPI.Models
+{
+    public class DeliveryExecutive
+    {
+        [Key]
+        public int executiveID { get; set; }
+        public string name { get; set; }
+        public string username { get; set; }
+        public string password { get; set; }
+        public int age { get; set; }
+        public string phone { get; set; }
+        public string address { get; set; }
+        public string city { get; set; }
+        public string isVerified { get; set; }
+    }
+}
